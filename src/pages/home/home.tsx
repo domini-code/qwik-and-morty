3baa10edaf69d40692e7d4ed516cc22b09d2732a
bezael <bezael@gmail.com>
2022-12-21 16:50:30 +0100

    feat(home): add home component

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,26 @@
+import { component$, Resource, useResource$ } from '@builder.io/qwik';
+import { apiService } from '~/api/fetch-data';
+import { Character } from '~/models/character';
+import CharacterComponent from '~/components/character/character';
+
+export default component$(() => {
+  const useResource = useResource$(() => apiService());
+
+  return (
+    <Resource
+      value={useResource}
+      onPending={() => <p>Loading characters..... </p>}
+      onResolved={({ results }) => {
+        return (
+          <>
+            <ul class='grid grid-cols-3 gap-1'>
+              {results.map((character: Character) => {
+                return <CharacterComponent {...character} />;
+              })}
+            </ul>
+          </>
+        );
+      }}
+    />
+  );
+});
